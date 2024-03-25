5e64cc795c877925bb1d334b429e7bd31e09243b
Sebastian Poma <73948362+Pomalec@users.noreply.github.com>
2024-03-25 19:36:00 +0000

    Add files via upload

---
+++ Assets/Scripts/Player/controller/inputcontrol.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class inputcontrol : ScriptableObject
+{
+    public abstract float retrievmoveinput();
+    public abstract float retrievmoveupinput();
+    public abstract bool retrievejumpinput();
+}
